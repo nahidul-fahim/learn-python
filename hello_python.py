df5d028710c0e045cd38171eacba45ca877f8a74
nahidul-fahim <nahidul7562@gmail.com>
2025-08-06 22:01:06 +0600

    Add new variables for Python learning progress

--- hello_python.py
+++ hello_python.py
@@ -4,3 +4,7 @@ def learn_python(n):
     print(n)
 
 learn_python("I'm learning Python")
+
+python_day2 = "Making my foundation strong"
+python_day3 = "Learning mutable and immutable data types"
+python_day4 = "Learning about functions"
